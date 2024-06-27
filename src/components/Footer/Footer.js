aef3c4ade69f6da22c6e27609b41a75347e9f6ba
WisdomPandovic <wisdompandovic@gmail.com>
2024-06-27 10:05:29 +0100

    second commit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from 'react';
 import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin, FaWeixin } from 'react-icons/fa';
+import Image from 'next/image';
 
 const Footer = () => {
     return (
@@ -28,22 +29,21 @@ const Footer = () => {
                 </div><hr></hr>
 
                 <div className="row">
-                    <div className="col-md-9">
+                    <div className="col-md-12">
                         <div className="row py-4">
-                            <div className="col-md-3">
+                            <div className="col-md-2">
                                 <div className="justify-content-center align-items-center pt-5 pb-5">
                                     <div>
-                                        <h5 className="text-left pb-3">Contact Us</h5>
-                                        <p>Phone: 081 2874 2738</p>
-                                        <p>Email: info@Rider.com</p>
-                                        <p>Location: Lorem ipsum dolor sit, amet consectetur adipisicing elit. In odit eaque at sequi! Vitae, repellendus.</p>
-                                        <div className="dk_foot_icon d-flex justify-content-center">
-                                            <div className="mx-2"><FaInstagram /></div>
-                                            <div className="mx-2"><FaFacebook /></div>
-                                            <div className="mx-2"><FaTwitter /></div>
-                                            <div className="mx-2"><FaLinkedin /></div>
-                                            <div className="mx-2"><FaWeixin /></div>
-                                        </div>
+                                        <Image
+                                            src="/images/sign.png"
+                                            alt="/"
+                                            width={10}
+                                            height={50}
+                                            className="card-img-top mb-3 white-image"
+                                        />
+                                        <p>SINC Partners Lab is a SaaS and a Marketing Tech startup studio building platform-based products for solution-providers and entrepreneurs that will help democratize success
+                                        </p>
+                                        
                                     </div>
                                 </div>
                             </div>
@@ -51,10 +51,13 @@ const Footer = () => {
                                 <div className="pt-5 pb-5">
                                     <h5 className='pb-3'>Platforms</h5>
                                     <ul className="list-unstyled">
-                                        <li className="mb-3">Lagos</li>
-                                        <li className="mb-3">Abuja</li>
-                                        <li className="mb-3">Calabar</li>
-                                        <li className="mb-3">All Cities</li>
+                                        <li className="mb-3">Kofoundme</li>
+                                        <li className="mb-3">InResidences</li>
+                                        <li className="mb-3">Service Market</li>
+                                        <li className="mb-3">Founder School</li>
+                                        <li className="mb-3">Metty</li>
+                                        <li className="mb-3">Grantty</li>
+                                        <li className="mb-3">SBO</li>
                                     </ul>
                                 </div>
                             </div>
@@ -62,51 +65,102 @@ const Footer = () => {
                                 <div className="pt-5 pb-5">
                                     <h5 className='pb-3'>Initiatives</h5>
                                     <ul className="list-unstyled">
-                                        <li className="mb-3">Business Class</li>
-                                        <li className="mb-3">First Class</li>
-                                        <li className="mb-3">Comfort Class</li>
-                                        <li className="mb-3">Mini & VIP VAN class</li>
-                                        <li className="mb-3"><a className="nav-link" href="/our-classes">All Classes</a></li>
+                                        <li className="mb-3">Jabi Plains</li>
+                                        <li className="mb-3">Local Pricing Initiative</li>
+                                        <li className="mb-3">Scholarship Program</li>
+                                        <li className="mb-3">School Pricing Program</li>
+                                        <li className="mb-3"><a className="nav-link" href="/our-classes">University STEM</a></li>
+                                        <li className="mb-3">University InResidency </li>
+                                        <li className="mb-3">Founder Festival </li>
                                     </ul>
                                 </div>
                             </div>
                             <div className="col-md-2">
                                 <div className="justify-content-center align-items-center pt-5 pb-5">
                                     <h5 className='pb-3'>About Us</h5>
-                                    <p>We love to share new offers and <br />Exclusive Promotions</p>
-                                    <form>
-                                        <div className="form-group">
-                                            <input type="email" className="form-control text-dark" id="email" name='email' placeholder="Your Email" required />
-                                            <button type="submit" className="btn custom-bg text-white mt-2">Subscribe</button>
-                                        </div>
-                                    </form>
+                                    <ul className="list-unstyled">
+                                        <li className="mb-3">Who We Are</li>
+                                        <li className="mb-3">Our People</li>
+                                        <li className="mb-3">Companies</li>
+                                        <li className="mb-3">Career</li>
+                                        <li className="mb-3">Resources</li>
+                                        <li className="mb-3">Blog & FAQ</li>
+                                        <li className="mb-3">Contact</li>
+                                    </ul>
                                 </div>
                             </div>
                             <div className="col-md-2">
                                 <div className="justify-content-center align-items-center pt-5 pb-5">
-                                    <h5 className='pb-3'>About Us</h5>
-                                    <p>We love to share new offers and <br />Exclusive Promotions</p>
-                                    <form>
-                                        <div className="form-group">
-                                            <input type="email" className="form-control text-dark" id="email" name='email' placeholder="Your Email" required />
-                                            <button type="submit" className="btn custom-bg text-white mt-2">Subscribe</button>
-                                        </div>
-                                    </form>
+                                    <h5 className='pb-3'>Others</h5>
+                                    <ul className="list-unstyled">
+                                        <li className="mb-3">SIP</li>
+                                        <li className="mb-3">SEEQ</li>
+                                        <li className="mb-3">Deals</li>
+                                        <li className="mb-3">Job</li>
+                                        <li className="mb-3">Investors</li>
+                                        <li className="mb-3">Hacker News</li>
+                                        <li className="mb-3">Press</li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div className="col-md-2 border-left" style={{ borderLeft: '2px solid white' }}>
+                                <div className="pt-5 pb-5 pl-4">
+                                    <h5 className='pb-3'>Locations</h5>
+                                    <ul className="list-unstyled">
+                                        <li className="mb-3">Abuja, Nigeria </li>
+                                        <li className="mb-3">Lagos, Nigeria </li>
+                                        <li className="mb-3">Philadelphia, USA</li>
+                                        <li className="mb-3">Mini & VIP VAN class</li>
+                                        <li className="mb-3"><a className="nav-link" href="/our-classes">All Classes</a></li>
+                                    </ul>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-3">
-                        <div className="pt-5 pb-5">
-                            <h5 className='pb-3'>Locations</h5>
-                            <ul className="list-unstyled">
-                                <li className="mb-3">Business Class</li>
-                                <li className="mb-3">First Class</li>
-                                <li className="mb-3">Comfort Class</li>
-                                <li className="mb-3">Mini & VIP VAN class</li>
-                                <li className="mb-3"><a className="nav-link" href="/our-classes">All Classes</a></li>
-                            </ul>
-                        </div>
+
+                </div><hr />
+            </div>
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-4'></div>
+                    <p className='col-lg-4 text-center'>You are guaranteed a 2x on your service or cash investment if you are first to invest</p>
+                    <div className='col-lg-4 d-flex align-items-center'>
+                        <Image
+                            src="/images/nike.png"
+                            alt="/"
+                            width={10}
+                            height={50}
+                            className="card-img-top mb-3 white-image"
+                        />
+                        <p style={{ marginLeft: '15px' }}>We are a business built on the foundation of Christian values and belief</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-4'>
+                        <p> ©
+                            2023 SINC Partners Lab Ltd. All rights reserved</p>
+                    </div>
+                    <div className='col-lg-4 text-center'>
+                        <ul className="list-unstyled d-flex">
+                            <li className="mb-3"><a className="text-white" href="">Privacy Policy</a></li>
+                            <li className="mb-3" style={{ marginLeft: '15px', marginRight: '15px' }}><a className="text-white" href="">Terms of Service</a></li>
+                            <li className="mb-3"><a className="text-white" href="">Security</a></li>
+                        </ul>
+
+                    </div>
+                    <div className='col-lg-4 '>
+                        <div className="dk_foot_icon d-flex justify-content-center align-items-center">
+                            <div className="mx-2"><FaInstagram /></div>
+                            <div className="mx-2"><FaFacebook /></div>
+                            <div className="mx-2"><FaTwitter /></div>
+                            <div className="mx-2"><FaLinkedin /></div>
+                            <div className="mx-2"><FaWeixin /></div>
+                        </div> 
                     </div>
                 </div>
             </div>
