57af210141d153f53e9455ff40f3a2b786378423
WisdomPandovic <wisdompandovic@gmail.com>
2024-06-27 14:56:32 +0100

    updated code

--- src/components/ConceptInnovationComponent/ConceptInnovation.js
+++ src/components/ConceptInnovationComponent/ConceptInnovation.js
@@ -9,12 +9,12 @@ const ConceptInnovation = () => {
                 <h2 className="text-center lh-1 fs-4 mb-4 mt-5">
                     Our 5 Concept Innovations
                 </h2>
-                <p className='pb-5'>ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <p className='pb-5'>We have five (5) proprietary concept innovations that will support the startup ecosystem, support solution-providers/entrepreneurs and ultimately help democratize success;</p>
             </div>
             <div className="row no-gutters">
                 <div className="col-lg-2 col-md-4 col-sm-6 col-12 mb-4">
                     <div className="card rounded-0" style={{ backgroundColor: '#0000000D' }}>
-                        <div className="card-body text-center">
+                        <div className="card-body text-center h-500">
                             <p className="card-text text-center fw-bold">Service Incubator</p>
                             <div className="d-flex justify-content-center mb-5">
                                 <Image
@@ -25,13 +25,13 @@ const ConceptInnovation = () => {
                                     className="rounded-circle"
                                 />
                             </div>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            <p>A service-for-shares model of incubating pre-product startups and early-stage companies by building their early stage product and MVP</p>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-2 col-md-4 col-sm-6 col-12 mb-4">
                     <div className="card rounded-0" style={{ backgroundColor: '#0000000D' }}>
-                        <div className="card-body">
+                        <div className="card-body text-center h-500">
                             <p className="card-text text-center fw-bold">Service Acceleration</p>
                             <div className="d-flex justify-content-center mb-5">
                                 <Image
@@ -42,13 +42,13 @@ const ConceptInnovation = () => {
                                     className="rounded-circle"
                                 />
                             </div>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            <p> A service-for-shares model for helping early stage startups with product and market validation scale with marketing using a media platform, influencer network or distribution network </p>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-2 col-md-4 col-sm-6 col-12 mb-4">
                     <div className="card rounded-0" style={{ backgroundColor: '#0000000D' }}>
-                        <div className="card-body">
+                        <div className="card-body text-center h-500">
                             <p className="card-text text-center fw-bold">Virtualting</p>
                             <div className="d-flex justify-content-center mb-5">
                                 <Image
@@ -59,13 +59,13 @@ const ConceptInnovation = () => {
                                     className="rounded-circle"
                                 />
                             </div>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            <p>A resource share model that allows companies share the time of resources (employees, consultants, technical advisers or influencers) while also splitting the service cost of the resource</p>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-2 col-md-4 col-sm-6 col-12 mb-4">
                     <div className="card rounded-0" style={{ backgroundColor: '#0000000D' }}>
-                        <div className="card-body">
+                        <div className="card-body text-center h-500">
                             <p className="card-text text-center fw-bold">Dimming</p>
                             <div className="d-flex justify-content-center mb-5">
                                 <Image
@@ -76,13 +76,13 @@ const ConceptInnovation = () => {
                                     className="rounded-circle"
                                 />
                             </div>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            <p>A progressive investment model that allows you to put aside income (disposable or non-disposable) as investment over a period towards an investment or spend of your choice </p>
                         </div>
                     </div>
                 </div>
                 <div className="col-lg-2 col-md-4 col-sm-6 col-12 mb-4">
                     <div className="card rounded-0" style={{ backgroundColor: '#0000000D' }}>
-                        <div className="card-body">
+                        <div className="card-body text-center h-500">
                             <p className="card-text text-center fw-bold">Mastathon</p>
                             <div className="d-flex justify-content-center mb-5">
                                 <Image
@@ -93,7 +93,7 @@ const ConceptInnovation = () => {
                                     className="rounded-circle"
                                 />
                             </div>
-                            <p>Sed ut perspiciatis unde omnis iste natus error sia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                            <p>A collaborative platform for experienced professionals to build, test, iterate  and get amazing ideas to be market ready in a couple of days (usually not more than a week or 60 hrs).</p>
                         </div>
                     </div>
                 </div>
